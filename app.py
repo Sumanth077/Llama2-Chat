5595537baa52aa9a425af5afea24fcc1d3eca86e
Sumanth <66694715+Sumanth077@users.noreply.github.com>
2023-08-22 18:40:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,30 @@ st.set_page_config(layout="wide")
 
 ClarifaiStreamlitCSS.insert_default_css(st)
 
-st.markdown("Please select a specific page from the sidebar to the left")
+from langchain.llms import Clarifai
+
+# Title & Sidebar Image
+
+st.title("🦙Chat with Llama 2 (70B) Model")
+st.sidebar.image("https://clarifai.com/favicon.svg", width=100)
+
+# Get the Clarifai API Key
+
+with st.sidebar:
+    clarifai_pat = st.text_input("Clarifai API Key", type="password")
+
+
+# Function to Generate Response
+
+def generate(text):
+    llm = Clarifai(pat=clarifai_pat, user_id='meta', app_id='Llama-2', model_id='llama2-70b-chat')
+    st.info(llm(text))
+
+# Form that takes text as imput and return the response from the Model
+with st.form("my_form"):
+    text = st.text_area("Enter text:", "How to Evaluate ML Models?")
+    submitted = st.form_submit_button("Submit")
+    if not clarifai_pat:
+        st.info("Please add your Clarifai PAT to continue.")
+    elif submitted:
+        generate(text)
